c4c279cead4c1395d7041d46b696cd3df36470f4
= <=>
2024-12-31 11:08:39 -0500

    Exception handling and modified calculator

--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -1,96 +1,115 @@
 ﻿// Welcome Message
 Console.WriteLine("***** - Welcome to the sample calculator! - ******");
 
-// Show calculator options / Show menu
-Console.WriteLine("Please select an operation (-1 to exit program)");
-Console.WriteLine("1. Addition");
-Console.WriteLine("2. Subtraction");
-Console.WriteLine("3. Multiplication");
-Console.WriteLine("4. Division");
-Console.WriteLine("5. Fibonacci sequence");
-int choice = Convert.ToInt32(Console.ReadLine());
-while(choice != -1)
+// Variable declarations
+int choice = 0;
+int num1, num2 = 0;
+int answer = 0;
+
+while (choice != -1)
 {
-    // Prompt for user input
-    Console.Write("Please enter the first number: ");
-    int num1 = Convert.ToInt32(Console.ReadLine());
-
-    Console.Write("Please enter the second number: ");
-    int num2 = Convert.ToInt32(Console.ReadLine());
-
-
-    // Decide which operation is needed based on selection option
-    //if(choice == 1)
-    //{
-    //    int sum = num1 + num2;
-    //    Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
-    //}
-    //else if (choice == 2)
-    //{
-    //    int difference = num1 - num2;
-    //    Console.WriteLine($"The difference of {num1} and {num2} is {difference}");
-    //}
-    //else if (choice == 3)
-    //{
-    //    int product = num1 * num2;
-    //    Console.WriteLine($"The product of {num1} and {num2} is {product}");
-    //}
-    //else if (choice == 4)
-    //{
-    //    int quotient = num1 / num2;
-    //    Console.WriteLine($"The quotient of {num1} and {num2} is {quotient}");
-    //}
-    //else
-    //{
-    //    Console.WriteLine("Invalid selection");
-    //}
-    int answer = 0;
-
-    switch (choice)
+    try
     {
-        case 1:
-            int sum = num1 + num2;
-            answer = sum;
-            Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
+        // Show calculator options / Show menu
+        Console.WriteLine("Please select an operation (-1 to exit program)");
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Subtraction");
+        Console.WriteLine("3. Multiplication");
+        Console.WriteLine("4. Division");
+        Console.WriteLine("5. Fibonacci sequence");
+      
+        choice = Convert.ToInt32(Console.ReadLine());
+
+        if (choice == -1)
+        {
             break;
-        case 2:
-            int difference = num1 - num2;
-            answer = difference;
-            Console.WriteLine($"The difference of {num1} and {num2} is {difference}");
-            break;
-        case 3:
-            int product = num1 * num2;
-            answer = product;
-            Console.WriteLine($"The product of {num1} and {num2} is {product}");
-            break;
-        case 4:
-            int quotient = num1 / num2;
-            answer = quotient;
-            Console.WriteLine($"The quotient of {num1} and {num2} is {quotient}");
-            break;
-        case 5:
-            Console.WriteLine("Fibonacci sequence");
-            for (int i = num1; i <= num2; i++)
-            {
-                answer += i;
-            }
-            break;
-        default:
-            Console.WriteLine("Invalid selection");
-            break;
+        }
+
+        // Prompt for user input
+        Console.Write("Please enter the first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Please enter the second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        // Decide which operation is needed based on selection option
+        //if(choice == 1)
+        //{
+        //    int sum = num1 + num2;
+        //    Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
+        //}
+        //else if (choice == 2)
+        //{
+        //    int difference = num1 - num2;
+        //    Console.WriteLine($"The difference of {num1} and {num2} is {difference}");
+        //}
+        //else if (choice == 3)
+        //{
+        //    int product = num1 * num2;
+        //    Console.WriteLine($"The product of {num1} and {num2} is {product}");
+        //}
+        //else if (choice == 4)
+        //{
+        //    int quotient = num1 / num2;
+        //    Console.WriteLine($"The quotient of {num1} and {num2} is {quotient}");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Invalid selection");
+        //}
+      
+
+        switch (choice)
+        {
+            case 1:
+                int sum = num1 + num2;
+                answer = sum;
+                Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
+                break;
+            case 2:
+                int difference = num1 - num2;
+                answer = difference;
+                Console.WriteLine($"The difference of {num1} and {num2} is {difference}");
+                break;
+            case 3:
+                int product = num1 * num2;
+                answer = product;
+                Console.WriteLine($"The product of {num1} and {num2} is {product}");
+                break;
+            case 4:
+                int quotient = num1 / num2;
+                answer = quotient;
+                Console.WriteLine($"The quotient of {num1} and {num2} is {quotient}");
+                break;
+            case 5:
+                Console.WriteLine("Fibonacci sequence");
+                for (int i = num1; i <= num2; i++)
+                {
+                    answer += i;
+                }
+                break;
+            default:
+                throw new Exception("Invalid menu selection");  
+        }
+        // Print global output
+        Console.WriteLine($"The result is: {answer}");
     }
-    // Print global output
-    Console.WriteLine($"The result is: {answer}");
-    Console.WriteLine("Press enter to continue");
-    Console.ReadLine();
-    Console.Clear();
-    Console.WriteLine("Please select an operation (-1 to exit program)");
-    Console.WriteLine("1. Addition");
-    Console.WriteLine("2. Subtraction");
-    Console.WriteLine("3. Multiplication");
-    Console.WriteLine("4. Division");
-    Console.WriteLine("5. Fibonacci sequence");
-    choice = Convert.ToInt32(Console.ReadLine());
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("Cannot divide by zero");  
+        continue;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        continue;
+    }
+    finally
+    {
+        Console.WriteLine("Press enter to continue");
+        Console.ReadLine();
+        Console.Clear();
+    }
 }
 
 Console.WriteLine("******** - Thank you for using the sample calculator! - *********");
