d8a46a949e7965280c94402b679699bb29f62051
= <=>
2024-12-31 10:18:01 -0500

    Added loop demos and enhanced calculator

---
+++ ConsoleApp.Loops/Program.cs
@@ -0,0 +1,68 @@
+﻿// Simple For Loop Demo
+
+// Print Hello World 10 times
+//Console.WriteLine("How many times would you like to print 'Hello World'?");
+//int times = Convert.ToInt32(Console.ReadLine());
+
+//for (int i = 0; i < times; i++)
+//{
+//    Console.WriteLine($"Hello World - {i + 1}");
+//}
+
+// While Loop
+Console.WriteLine("*********************While Loop*********************");
+Console.WriteLine("How many times would you like to print 'Hello World'?");
+int times = Convert.ToInt32(Console.ReadLine());
+int counter = 0;
+while (counter < times)
+{
+    Console.WriteLine($"Hello World - {counter + 1}");
+    counter++;
+}
+int sum = 0;
+int num = 0;
+
+while(num != -1)
+{
+    Console.Write("Please enter number to be summed up. (-1 to stop or exit): ");
+    num = Convert.ToInt32(Console.ReadLine());
+    if(num!= -1)
+    {
+        sum += num;
+    }
+   
+}
+Console.WriteLine($"The sum of the numbers entered is {sum}");
+Console.WriteLine("*****************End While Loop*********************");
+
+// Do While Loop
+Console.WriteLine("*********************Do While Loop*********************");
+sum = 0;
+num = 0;
+
+do
+{
+    Console.Write("Please enter number to be summed up. (-1 to stop or exit): ");
+    num = Convert.ToInt32(Console.ReadLine());
+    if (num != -1)
+    {
+        sum += num;
+    }
+}
+while (num != -1);
+Console.WriteLine($"The sum of the numbers entered is {sum}");
+Console.WriteLine("*****************End Do While Loop*********************");
+
+/*
+ * ===============Keywords=================
+ * Increment: count by static value (++ operator)
+ * Accumulate: add up values (compound assignments)
+ * Scope: Lifetime of the variable
+ * Iteration: Each time the loop repeats
+ * Nested: Having control structure inside another control structure
+ * break: syntax used to stop statements in a control structure. 
+ *          - Used in switch...case statements
+ *          - Can be used in loopes to break iteration.
+ * continue - syntax to skip over lines and fast-forward to the end of the code block. Can be used in loops to skip iteration.
+ * 
+ */
--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -1,75 +1,96 @@
 ﻿// Welcome Message
 Console.WriteLine("***** - Welcome to the sample calculator! - ******");
 
-// Prompt for user input
-Console.Write("Please enter the first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Please enter the second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-
 // Show calculator options / Show menu
-Console.WriteLine("Please select an operation");
+Console.WriteLine("Please select an operation (-1 to exit program)");
 Console.WriteLine("1. Addition");
 Console.WriteLine("2. Subtraction");
 Console.WriteLine("3. Multiplication");
 Console.WriteLine("4. Division");
-
+Console.WriteLine("5. Fibonacci sequence");
 int choice = Convert.ToInt32(Console.ReadLine());
-
-// Decide which operation is needed based on selection option
-//if(choice == 1)
-//{
-//    int sum = num1 + num2;
-//    Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
-//}
-//else if (choice == 2)
-//{
-//    int difference = num1 - num2;
-//    Console.WriteLine($"The difference of {num1} and {num2} is {difference}");
-//}
-//else if (choice == 3)
-//{
-//    int product = num1 * num2;
-//    Console.WriteLine($"The product of {num1} and {num2} is {product}");
-//}
-//else if (choice == 4)
-//{
-//    int quotient = num1 / num2;
-//    Console.WriteLine($"The quotient of {num1} and {num2} is {quotient}");
-//}
-//else
-//{
-//    Console.WriteLine("Invalid selection");
-//}
-int answer = 0;
-
-switch (choice)
+while(choice != -1)
 {
-    case 1:
-        int sum = num1 + num2;
-        answer = sum;
-        Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
-        break;
-    case 2:
-        int difference = num1 - num2;
-        answer = difference;
-        Console.WriteLine($"The difference of {num1} and {num2} is {difference}");
-        break;
-    case 3:
-        int product = num1 * num2;
-        answer = product;
-        Console.WriteLine($"The product of {num1} and {num2} is {product}");
-        break;
-    case 4:
-        int quotient = num1 / num2;
-        answer = quotient;
-        Console.WriteLine($"The quotient of {num1} and {num2} is {quotient}");
-        break;
-    default:
-        Console.WriteLine("Invalid selection");
-        break;
-}
-// Print global output
-Console.WriteLine($"The result is: {answer}");
+    // Prompt for user input
+    Console.Write("Please enter the first number: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
 
+    Console.Write("Please enter the second number: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+    // Decide which operation is needed based on selection option
+    //if(choice == 1)
+    //{
+    //    int sum = num1 + num2;
+    //    Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
+    //}
+    //else if (choice == 2)
+    //{
+    //    int difference = num1 - num2;
+    //    Console.WriteLine($"The difference of {num1} and {num2} is {difference}");
+    //}
+    //else if (choice == 3)
+    //{
+    //    int product = num1 * num2;
+    //    Console.WriteLine($"The product of {num1} and {num2} is {product}");
+    //}
+    //else if (choice == 4)
+    //{
+    //    int quotient = num1 / num2;
+    //    Console.WriteLine($"The quotient of {num1} and {num2} is {quotient}");
+    //}
+    //else
+    //{
+    //    Console.WriteLine("Invalid selection");
+    //}
+    int answer = 0;
+
+    switch (choice)
+    {
+        case 1:
+            int sum = num1 + num2;
+            answer = sum;
+            Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
+            break;
+        case 2:
+            int difference = num1 - num2;
+            answer = difference;
+            Console.WriteLine($"The difference of {num1} and {num2} is {difference}");
+            break;
+        case 3:
+            int product = num1 * num2;
+            answer = product;
+            Console.WriteLine($"The product of {num1} and {num2} is {product}");
+            break;
+        case 4:
+            int quotient = num1 / num2;
+            answer = quotient;
+            Console.WriteLine($"The quotient of {num1} and {num2} is {quotient}");
+            break;
+        case 5:
+            Console.WriteLine("Fibonacci sequence");
+            for (int i = num1; i <= num2; i++)
+            {
+                answer += i;
+            }
+            break;
+        default:
+            Console.WriteLine("Invalid selection");
+            break;
+    }
+    // Print global output
+    Console.WriteLine($"The result is: {answer}");
+    Console.WriteLine("Press enter to continue");
+    Console.ReadLine();
+    Console.Clear();
+    Console.WriteLine("Please select an operation (-1 to exit program)");
+    Console.WriteLine("1. Addition");
+    Console.WriteLine("2. Subtraction");
+    Console.WriteLine("3. Multiplication");
+    Console.WriteLine("4. Division");
+    Console.WriteLine("5. Fibonacci sequence");
+    choice = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("******** - Thank you for using the sample calculator! - *********");
